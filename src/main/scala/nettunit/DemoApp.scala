74a6b58e8bf5f479af229b885dd475efe5d5f9d5
Davide Andrea Guastella <davide.guastella@icar.cnr.it>
2022-10-15 13:45:42 +0200

    Added app launcher

---
+++ src/main/scala/nettunit/DemoApp.scala
@@ -0,0 +1,27 @@
+package nettunit
+
+import javafx.fxml.FXMLLoader
+import scalafx.Includes._
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+import scalafxml.core.{FXMLView, NoDependencyResolver}
+
+import java.io.IOException
+
+object DemoApp extends JFXApp3 {
+  override def start(): Unit = {
+
+    val resource = getClass.getResource("demoUI.fxml")
+    if (resource == null) {
+      throw new IOException("Cannot load resource: demoUI.fxml")
+    }
+
+    val root = FXMLView(resource, NoDependencyResolver)
+
+    stage = new JFXApp3.PrimaryStage() {
+      title = "NETTUNIT"
+      scene = new Scene(root)
+
+    }
+  }
+}
